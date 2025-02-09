3ce6bc5acba8cead7acf6e58ab51293a8a4e88d6
SleepsOne <DucLH.B21CN034@stu.ptit.edu.vn>
2025-02-09 22:29:28 +0700

    add database setup and update server to serve static files

--- src/server.js
+++ src/server.js
@@ -1,10 +1,21 @@
 import express from "express";
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5004;
+
+// lay duong dan hien tai cua module
+const __filename = fileURLToPath(import.meta.url);
+// lay duong dan thu muc cha cua file module
+const __dirname = dirname(__filename);
+
+app.use(express.json());
+// middlle ware, xu li cac file tinh trong thu muc public de render tuong ung
+app.use(express.static(path.join(__dirname, "../public")));
 
 app.get("/", (req, res) => {
-  res.send("OK!");
+  res.sendFile("index.html");
   res.sendStatus(200);
 });
 
