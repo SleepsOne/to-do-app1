546ddb5b8b3f983325453083e5919259120fd222
SleepsOne <DucLH.B21CN034@stu.ptit.edu.vn>
2025-02-10 11:44:50 +0700

    add authentication and todo routes with registration and login functionality

--- src/server.js
+++ src/server.js
@@ -1,6 +1,8 @@
 import express from "express";
 import path, { dirname } from "path";
 import { fileURLToPath } from "url";
+import authRoutes from "./routes/authRoutes.js";
+import todoRoutes from "./routes/todoRoutes.js";
 
 const app = express();
 const PORT = process.env.PORT || 5004;
@@ -10,6 +12,7 @@ const __filename = fileURLToPath(import.meta.url);
 // lay duong dan thu muc cha cua file module
 const __dirname = dirname(__filename);
 
+// MiddleWare
 app.use(express.json());
 // middlle ware, xu li cac file tinh trong thu muc public de render tuong ung
 app.use(express.static(path.join(__dirname, "../public")));
@@ -18,6 +21,10 @@ app.get("/", (req, res) => {
   res.sendFile("index.html");
   res.sendStatus(200);
 });
+
+// Routes
+app.use("/auth", authRoutes);
+app.use("/todos", todoRoutes);
 
 app.listen(PORT, () => {
   console.log("Server starting on port " + PORT);
